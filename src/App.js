f32bbbf5eb997fce0f30f288657a95dec282a62d
Jugariu Raul <jugariuraul1@gmail.com>
2026-04-30 16:43:04 +0300

    fix eslint useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar';
 import MovieCard from './components/MovieCard';
